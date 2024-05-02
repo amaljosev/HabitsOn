ea6dedc80f3a4e1c227ca98558653d4a4a5230e4
amaljose <amaljvattakkunnel@gmail.com>
2024-05-02 20:18:07 +0530

    icon_luncher added

--- lib/view/screens/forms/start_default_habit.dart
+++ lib/view/screens/forms/start_default_habit.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:habitson/controller/new_habits_controller.dart';
 import 'package:habitson/controller/started_habit_controller.dart';
--- lib/view/screens/habit/started_habit_screen.dart
+++ lib/view/screens/habit/started_habit_screen.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:habitson/controller/activities_controller.dart';
 import 'package:habitson/controller/habit_operations.dart';
